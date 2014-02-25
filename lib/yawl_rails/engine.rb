2ae98e0381281c43663a4de2622e2d943b08b081
Ricardo Chimal, Jr <kiwi@null.cx>
2014-02-25 02:45:44 -0800

    just make up your mind...

--- lib/yawl_rails/engine.rb
+++ lib/yawl_rails/engine.rb
@@ -13,7 +13,7 @@ module YawlRails
 
     initializer "yawl_rails.append_migrations" do |app|
       unless app.root.to_s.match root.to_s
-        app.config.paths["db/migrate"] << config.paths["db/migrate"].expanded
+        app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded
       end
     end
 
