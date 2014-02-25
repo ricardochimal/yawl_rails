6456c8ec93bb4e73d24e56d92dee93ea4eb51891
Ricardo Chimal, Jr <kiwi@null.cx>
2014-02-24 19:14:17 -0800

    add initial steps controller spec

---
+++ spec/controllers/yawl_rails/steps_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+module YawlRails
+  describe StepsController do
+    before(:each) do
+      @process_id = Yawl::Process.insert(:desired_state => "testing")
+      @step_id = Yawl::Step.insert(:process_id => @process_id, :name => "step1", :seq => 1)
+    end
+
+    describe "GET #show" do
+      it "renders the right template" do
+        get :show, :process_id => @process_id, :id => @step_id, :use_route => :yawl_rails
+
+        expect(response).to be_success
+        expect(response).to render_template("show")
+      end
+
+      it "assigns @step" do
+        get :show, :process_id => @process_id, :id => @step_id, :use_route => :yawl_rails
+
+        assigns(:step).id.should eq(@step_id)
+      end
+    end
+  end
+end
