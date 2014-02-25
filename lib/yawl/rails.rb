4cf91b724e721c52344bc82df37382de7f2ce635
Ricardo Chimal, Jr <kiwi@null.cx>
2014-02-24 19:02:51 -0800

    require yawl/rails in an initializer, and require haml for templates

--- lib/yawl/rails.rb
+++ lib/yawl/rails.rb
@@ -1,4 +1,2 @@
 require "yawl"
 require "yawl/process_active_record"
-
-require "yawl_rails"
--- lib/yawl_rails/engine.rb
+++ lib/yawl_rails/engine.rb
@@ -1,3 +1,5 @@
+require "haml"
+
 module YawlRails
   class Engine < ::Rails::Engine
     isolate_namespace YawlRails
@@ -8,5 +10,15 @@ module YawlRails
     end
 
     config.active_record.schema_format = :sql
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded
+      end
+    end
+
+    initializer :setup_yawl do |app|
+      require "yawl/rails"
+    end
   end
 end
