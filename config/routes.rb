5cd6145593c1b4d3e03f48f181ddf7b1e2c72327
Ricardo Chimal, Jr <kiwi@null.cx>
2014-02-24 21:03:57 -0800

    update config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 YawlRails::Engine.routes.draw do
-  scope module: 'yawl_rails', as: 'yawl' do
-    resources :processes, :only => [:index, :show] do
-      member do
-        post :restart
-        get :steps
-      end
-      resources :steps, :only => [:show]
+  resources :processes, :only => [:index, :show] do
+    member do
+      post :restart
+      get :steps
     end
+    resources :steps, :only => [:show]
   end
 end
